8d14dfce5df19c143714551c5f385f8eb34d7210
Nana Sakisaka <saki7@setna.io>
2017-08-26 09:31:08 +0900

    input field select/focus

--- README.md
+++ README.md
@@ -21,13 +21,13 @@
     var cs = new CRSearch;
     cs.database("https://cpprefjp.github.io");
     cs.database("https://boostjp.github.io");
-    cs.searchbox("#crsearch");
+    cs.searchbox(".crsearch");
   });
 --></script>
 ```
 
 ```html
-<div id="crsearch"></div>
+<div class="crsearch"></div>
 ```
 
 see also: [自分のサービスをcrsearchに対応させる方法](https://github.com/cpprefjp/crsearch/wiki/%E8%87%AA%E5%88%86%E3%81%AE%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9%E3%82%92crsearch%E3%81%AB%E5%AF%BE%E5%BF%9C%E3%81%95%E3%81%9B%E3%82%8B%E6%96%B9%E6%B3%95)
--- src/js/crsearch.js
+++ src/js/crsearch.js
@@ -1,28 +1,43 @@
-const $ = require('jquery');
+const $ = require('jquery')
+const Mousetrap = require('mousetrap')
 
 class CRSearch {
   static OPTS_DEFAULT = {
     klass: {
       search_button: 'glyphicon glyphicon-search',
     },
-  };
+  }
 
   constructor(opts = CRSearch.OPTS_DEFAULT) {
-    this.opts = opts;
-    this.databases = [];
+    this.opts = opts
+    this.databases = []
+
+    Mousetrap.bind('/', function() {
+      return this.select_default()
+    }.bind(this))
   }
 
   database(base_url) {
-    this.databases.push(base_url);
+    this.databases.push(base_url)
   }
 
   searchbox(sel) {
-    let box = $(sel);
-    box.append('<div class="control" />');
+    let box = $(sel)
+    let control = $('<div class="control" />')
+    control.appendTo(box)
 
-    let control = box.children('.control');
-    control.append('<input type="text" class="input">');
+    let input = $('<input type="text" class="input">')
+    input.appendTo(control)
+    input.on('click', function() {
+      return this.select_default()
+    }.bind(this))
+    this.default_input = input
+  }
+
+  select_default() {
+    this.default_input.select()
+    return false
   }
 }
-module.exports = CRSearch;
+module.exports = CRSearch
 
