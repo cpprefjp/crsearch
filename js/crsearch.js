001f27a35795127af3fc195f4f18184365bf68c9
Nana Sakisaka <saki7@setna.io>
2017-10-29 04:32:30 +0900

    v1.2.0

--- js/crsearch.js
+++ js/crsearch.js
@@ -8,7 +8,7 @@ import {Logger} from './crsearch/logger'
 
 export default class CRSearch {
   static APPNAME = 'CRSearch'
-  static VERSION = '1.1.2'
+  static VERSION = '1.2.0'
   static HOMEPAGE = 'https://github.com/cpprefjp/crsearch'
 
   static OPTS_DEFAULT = {
