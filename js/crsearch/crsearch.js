16af672b086f92d3b5efb84031088adaf580894c
Nana Sakisaka <saki7@setna.io>
2017-10-31 17:35:12 +0900

    v1.4.1

--- js/crsearch/crsearch.js
+++ js/crsearch/crsearch.js
@@ -9,7 +9,7 @@ import {Index} from './index'
 
 class CRSearch {
   static APPNAME = 'CRSearch'
-  static VERSION = '1.4.0'
+  static VERSION = '1.4.1'
   static HOMEPAGE = 'https://github.com/cpprefjp/crsearch'
 
   static OPTS_DEFAULT = {
