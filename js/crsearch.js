3998cc31ce306b042f109308b545db53d77d4ea1
Nana Sakisaka <saki7@setna.io>
2017-11-06 18:48:01 +0900

    Export all classes

--- js/crsearch.js
+++ js/crsearch.js
@@ -1,5 +1,12 @@
-export {CRSearch} from './crsearch/crsearch'
+export {IndexType} from './crsearch/index-type'
+export {IndexID} from './crsearch/index-id'
+export {Index} from './crsearch/index'
+export {Namespace} from './crsearch/namespace'
+export {Database} from './crsearch/database'
+
 import * as KC from './crsearch/kunai-config'
 
 export {KC}
+
+export {CRSearch} from './crsearch/crsearch'
 
