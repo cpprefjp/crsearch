baf6a9ac5101c335e08355bfd605e5521d43add7
Mitsuru Kariya <kariya_mitsuru@hotmail.com>
2018-06-08 11:01:25 +0900

    コンテナ起動時に /var/src をカレントディレクトリになるように修正
    コンテナ起動時にいちいち cd /var/src するのが面倒なので、Dockerfile 側で
    WORKDIR /var/src を指定しました。

--- docker.sh
+++ docker.sh
@@ -24,13 +24,13 @@ DOCKER_RM=${DOCKER_RM---rm}
 
 case "$1" in
   "build" ) docker build -t crsearch:0.0.0-alpine docker ;;
-  "install" ) docker run $DOCKER_RM -v `pwd`:/var/src $DOCKER_IT crsearch:0.0.0-alpine /bin/sh -c "cd /var/src && exec npm install" ;;
+  "install" ) docker run $DOCKER_RM -v `pwd`:/var/src $DOCKER_IT crsearch:0.0.0-alpine npm install ;;
   "run" )
     if [ $# -lt 2 ]; then
       show_help
       exit 1
     fi
-    docker run $DOCKER_RM -v `pwd`:/var/src -p 8080:8080 $DOCKER_IT crsearch:0.0.0-alpine /bin/sh -c "cd /var/src && exec npm run $2" ;;
+    docker run $DOCKER_RM -v `pwd`:/var/src -p 8080:8080 $DOCKER_IT crsearch:0.0.0-alpine npm run $2 ;;
   * )
     show_help
     exit 1 ;;
