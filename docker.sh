af2a74f7a73ca7f04a2d92e181a5002e1a0ede87
Akira Takahashi <faithandbrave@gmail.com>
2022-11-06 12:12:10 +0900

    依存ライブラリを更新

--- docker.sh
+++ docker.sh
@@ -5,6 +5,7 @@ function show_help() {
   echo "$0 install"
   echo "$0 run dev"
   echo "$0 run build"
+  echo "$0 audit-fix"
 }
 
 if [ $# -lt 1 ]; then
@@ -25,6 +26,7 @@ DOCKER_RM=${DOCKER_RM---rm}
 case "$1" in
   "build" ) docker build -t crsearch:0.0.0-alpine docker ;;
   "install" ) docker run $DOCKER_RM -v `pwd`:/var/src $DOCKER_IT crsearch:0.0.0-alpine npm install ;;
+  "audit-fix" ) docker run $DOCKER_RM -v `pwd`:/var/src $DOCKER_IT crsearch:0.0.0-alpine npm audit fix ;;
   "run" )
     if [ $# -lt 2 ]; then
       show_help
